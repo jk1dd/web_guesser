358d712e767991db996e0691d3f0b183238c5e5f
jk1dd <portajonk@gmail.com>
2017-03-06 12:06:54 -0700

    added background based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,19 +6,24 @@ SECRET_NUMBER = rand(0..100)
 get '/' do
   guess = params["guess"].to_i
   message = checker(guess)
-  erb :index, :locals => {:guess => guess, :message => message}
+  erb :index, :locals => {:background => @background, :guess => guess, :message => message}
 end
 
 def checker(guess)
   if guess > (SECRET_NUMBER + 5)
+    @background = "red"
     "Way too high!"
   elsif guess < (SECRET_NUMBER - 5)
+    @background = "red"
     "Way too low!"
   elsif guess > SECRET_NUMBER
+    @background = "pink"
     "Too high!"
   elsif guess < SECRET_NUMBER
+    @background = "pink"
     "Too low!"
   else
+    @background = "green"
     "You got it right!\nThe secret number is #{SECRET_NUMBER}."
   end
 end
